bedf757886d9e8856a6767b41c7bf5be8d76f92d
Kazi Md. Tawsif Rahman <69233243+tawsif113@users.noreply.github.com>
2026-09-10 15:41:09 +0600

    feat(x): register X configuration properties

--- src/main/java/com/fbscraper/App.java
+++ src/main/java/com/fbscraper/App.java
@@ -1,12 +1,13 @@
 package com.fbscraper;
 
 import com.fbscraper.config.AppConfig;
+import com.fbscraper.config.XConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(AppConfig.class)
+@EnableConfigurationProperties({AppConfig.class, XConfig.class})
 public class App {
 
     public static void main(String[] args) {
